1ec0657964c2508f5a5d9971cd4ac754a41aed08
Cody <yu0607168@gmail.com>
2025-04-27 21:13:52 +0800

    Update print.go

--- simpleprint/print.go
+++ simpleprint/print.go
@@ -6,26 +6,26 @@ import (
 )
 
 // Println 包装了fmt.Println函数，可以直接使用
-func println(a ...interface{}) (n int, err error) {
+func Println(a ...interface{}) (n int, err error) {
 	return fmt.Println(a...)
 }
 
 // Printf 包装了fmt.Printf函数，可以直接使用
-func printf(format string, a ...interface{}) (n int, err error) {
+func Printf(format string, a ...interface{}) (n int, err error) {
 	return fmt.Printf(format, a...)
 }
 
 // Print 包装了fmt.Print函数，可以直接使用
-func print(a ...interface{}) (n int, err error) {
+func Print(a ...interface{}) (n int, err error) {
 	return fmt.Print(a...)
 }
 
 // Sprintf 包装了fmt.Sprintf函数，可以直接使用
-func sprintf(format string, a ...interface{}) string {
+func Sprintf(format string, a ...interface{}) string {
 	return fmt.Sprintf(format, a...)
 }
 
 // Fprintf 包装了fmt.Fprintf函数，可以直接使用
-func fprintf(w io.Writer, format string, a ...interface{}) (n int, err error) {
+func Fprintf(w io.Writer, format string, a ...interface{}) (n int, err error) {
 	return fmt.Fprintf(w, format, a...)
 } 
